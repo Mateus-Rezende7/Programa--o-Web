a3efc85c0c67072bc0049b5f745895d549d3a694
Mateus Rezende de Oliveira <mateusrezendedeoliveira@gmail.com>
2022-06-14 23:25:42 -0300

    Exercício 04 react

---
+++ React/exercicio_aula_04/src/App.js
@@ -0,0 +1,17 @@
+
+import { Component } from 'react';
+import Notas from './Components/Notas';
+import Nome from './Components/Nome';
+
+export class App extends Component {
+    render() {
+      return (
+        <div className='container'>
+          <Nome />
+          <Notas />
+        </div>
+      );
+    }
+  }
+  
+  export default App;
