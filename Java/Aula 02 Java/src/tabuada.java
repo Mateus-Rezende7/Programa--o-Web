8d24913b52172385b60a25d1cdd7d95be6ad87af
Mateus Rezende de Oliveira <mateusrezendedeoliveira@gmail.com>
2022-07-01 18:59:04 -0300

    Java e tcc

---
+++ Java/Aula 02 Java/src/tabuada.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class tabuada {
+    public static void main(String[] args) {
+        int i = 1;
+     System.out.println("¨¨¨¨¨Tabuada do 5¨¨¨¨¨");
+        do {
+            System.out.println("5 x "+i+" = "+i*5);
+            i++;
+        }while (i<=10);
+
+    }
+}
